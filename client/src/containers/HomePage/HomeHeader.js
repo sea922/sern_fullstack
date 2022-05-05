9938377e5fbdf91df12c24308091984292615b3b
I99900K <truongvans1010@gmail.com>
2022-05-05 08:38:17 +0700

    change language with Redux

--- client/src/containers/HomePage/HomeHeader.js
+++ client/src/containers/HomePage/HomeHeader.js
@@ -41,56 +41,57 @@ class HomeHeader extends Component {
                             <div className='sub-title'><FormattedMessage id="home-header.search-doctor" /></div>
                         </div>
                         <div className='child-content'>
-                            <div><b>Cơ sở y tế</b></div>
-                            <div className='sub-title'>Chọn bệnh viện phòng khám</div>
+                            <div><b><FormattedMessage id="home-header.health-facilities" /></b></div>
+                            <div className='sub-title'><FormattedMessage id="home-header.select-room" /></div>
                         </div>
                         <div className='child-content'>
-                            <div><b>Bác sĩ</b></div>
-                            <div className='sub-title'>Chọ bác sĩ giỏi</div>
+                            <div><b><FormattedMessage id="home-header.doctor" /></b></div>
+                            <div className='sub-title'><FormattedMessage id="home-header.select-doctor" /></div>
                         </div>
                         <div className='child-content'>
-                            <div><b>Gói khám</b></div>
-                            <div className='sub-title'>Khám sức khỏe tổng quát</div>
+                            <div><b><FormattedMessage id="home-header.fee" /></b></div>
+                            <div className='sub-title'><FormattedMessage id="home-header.check-health" /></div>
                         </div>
                     </div>
                     <div className='right-content'>
-                        <div className='support'><i className="fas fa-question-circle"></i>Hỗ trợ</div>
-                        <div className={language === LANGUAGES.VI ? 'language-vi active' : 'laguage-vi'}><span onClick={() => this.changeLanguage(LANGUAGES.VI)}>VN</span></div>
-                        <div className='language-en'><span onClick={() => this.changeLanguage(LANGUAGES.EN)}>EN</span></div>
+                        <div className='support'><i className="fas fa-question-circle"></i><FormattedMessage id="home-header.support" /></div>
+                        <div className={language === LANGUAGES.VI ? 'language-vi active' : 'language-vi'}><span onClick={() => this.changeLanguage(LANGUAGES.VI)}>VN</span></div>
+                        <div className={language === LANGUAGES.EN ? 'language-en active ' : 'language-en'}><span onClick={() => this.changeLanguage(LANGUAGES.EN)}>EN</span></div>
+                        
                     </div>  
                 </div>
             </div>
             <div className='home-header-banner'>
                 <div className='content-up'>
-                    <div className='title1'>NỀN TẢNG Y TẾ</div>
-                    <div className='title2'>CHĂM SÓC SỨC KHỎE TOÀN DIỆN</div>
+                    <div className='title1'><FormattedMessage id="banner.title1" /></div>
+                    <div className='title2'><FormattedMessage id="banner.title2" /></div>
                     <div className='search'>
                         <i className='fas fa-search'></i>
-                        <input type='text' placeholder='Tìm chuyên khoa'/>
+                        <input type='text' placeholder='Tìm chuyên khoa'/> 
                     </div>
                 </div>
                 <div className='content-down'>
                     <div className='options'>
                         <div className='options-child'>
                             <div className='icon-child'><i className="fas fa-hospital"></i></div>
-                            <div className='text-child'>Khám chuyên khoa</div>
+                            <div className='text-child'><FormattedMessage id="banner.child1" /></div>
                         </div>
                         <div className='options-child'>
                             <div className='icon-child'><i className="fas fa-mobile-alt"></i></div>
-                            <div className='text-child'>Khám từ xa</div>
+                            <div className='text-child'><FormattedMessage id="banner.child2" /></div>
                         </div>
                         <div className='options-child'>
                             <div className='icon-child'><i className="fas fa-procedures"></i></div>
-                            <div className='text-child'>Khám tổng quát</div>
+                            <div className='text-child'><FormattedMessage id="banner.child3" /></div>
                         </div><div className='options-child'>
                             <div className='icon-child'><i className="fas fa-flask"></i></div>
-                            <div className='text-child'>Xét nghiệm y học</div>
+                            <div className='text-child'><FormattedMessage id="banner.child4" /></div>
                         </div><div className='options-child'>
                             <div className='icon-child'><i className="fas fa-user-md"></i></div>
-                            <div className='text-child'>Sức khỏe tinh thần</div>
+                            <div className='text-child'><FormattedMessage id="banner.child5" /></div>
                         </div><div className='options-child'>
                             <div className='icon-child'><i className="fas fa-briefcase-medical"></i></div>
-                            <div className='text-child'>Khám nha khoa</div>
+                            <div className='text-child'><FormattedMessage id="banner.child6" /></div>
                         </div>
                     </div>
                 </div>
