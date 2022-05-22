1d66c6a4bab32b734eb35685b25e9fcead16e18f
I99900K <truongvans1010@gmail.com>
2022-05-22 16:04:11 +0700

    authorization menu admin+doctor

--- client/src/containers/Header/menuApp.js
+++ client/src/containers/Header/menuApp.js
@@ -17,9 +17,13 @@ export const adminMenu = [
                 // ]
             },
             // { name: 'menu.system.system-parameter.header', link: '/system/system-parameter' },
+            // {
+            //     name:  'menu.admin.manage-admin', link: '/system/user-admin'
+            // },
             {
-                name:  'menu.admin.manage-admin', link: '/system/user-admin'
-            },
+                //Quản lý kế hoạch khám bệnh của bác sĩ
+                name: 'menu.doctor.manage-schedule', link: '/doctor/manage-schedule'
+            }
             
         ]
     },
@@ -51,3 +55,12 @@ export const adminMenu = [
         ]
     },
 ];
+
+export const doctorMenu = [{
+    name: 'menu.admin.manage-user',
+    menus: [{
+        //Quản lý kế hoạch khám bệnh của bác sĩ
+        name: 'menu.doctor.manage-schedule',
+        link: '/doctor/manage-schedule'
+    }]
+}]; 
